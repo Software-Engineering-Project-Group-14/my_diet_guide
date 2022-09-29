5f26371fd5de1365d88d5c7c1d9af2b6ddd66a79
Jalitha99 <jalithakalsara2@gmail.com>
2022-09-29 13:09:02 +0530

    bmi/signup

--- lib/screens/check_bmi.dart
+++ lib/screens/check_bmi.dart
@@ -1,108 +1,179 @@
 import 'package:flutter/material.dart';
+import 'package:my_diet_guide/models/BMI.dart';
+import 'package:my_diet_guide/widgets/background_image.dart';
+
+import '../widgets/BMICard.dart';
 
 class CheckBMI extends StatefulWidget {
-  const CheckBMI({Key? key}) : super(key: key);
-
+   CheckBMI({Key? key}) : super(key: key);
+  double result = 0;
   @override
   State<CheckBMI> createState() => _CheckBMIState();
 }
 
 class _CheckBMIState extends State<CheckBMI> {
+  final _currentWeight = TextEditingController();
+  final _currentHeight = TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.indigo[800],
-        title: const Text('Check BMI',),
-      ),
-      body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
+    return Stack(children: [
+      BackgroundImage(),
+      Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.indigo[800],
+          title: const Text(
+            'Check BMI',
+          ),
+        ),
+        body: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  // Container(
+                  //   // child : BMICard(),
+                  //   child: Text('${result}',
+                  //   style: TextStyle(
+                  //     color: Colors.white
+                  //   ),),
+                  // ),
+                  // BMICard(result: '${result}'),
 
-                //current weight
-                Row(
-                  children: [
-                    //current weight
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text('cur. Weight : ',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      ),
+                  //bmi window
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade600.withOpacity(0.6),
+                      borderRadius: BorderRadius.circular(16),
                     ),
+                    width: 300,
+                    height: 100,
+                    child: Center(
+                      child: Text(
 
-                    //input current weight
-                    SizedBox(
-                      width: 250,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            labelText: 'Enter Weight'
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-
-                //current height
-                Row(
-                  children: [
-                    //current weight
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text('cur. Height : ',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-
-                    //input current weight
-                    SizedBox(
-                      width: 250,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              labelText: 'Enter Height'
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-
-                
-                SizedBox(height: 20,),
-                //calculate button
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: GestureDetector(
-                    child: Container(
-                      padding: EdgeInsets.all(25),
-                      decoration: BoxDecoration(color: Colors.deepPurple,
-                          borderRadius: BorderRadius.circular(12)),
-                      child: Center(
-                        child: Text('Calculate',
-                          style: TextStyle(color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18),),
+                        '${widget.result}',
+                        style: TextStyle(color: Colors.white),
                       ),
                     ),
                   ),
-                ),
-              ],
+
+                  SizedBox(
+                    height: 30,
+                  ),
+
+                  //current weight
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade600.withOpacity(0.6),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    width: 300,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                        child: TextField(
+                          style: TextStyle(
+                            fontSize: 22,
+                            color: Colors.white,
+                          ),
+                          controller: _currentWeight,
+                          decoration: InputDecoration(
+                            errorStyle: TextStyle(
+                                fontSize: 15, color: Colors.red[1000]),
+                            contentPadding: EdgeInsets.symmetric(vertical: 0),
+                            border: InputBorder.none,
+                            hintText: '   Current Weight (Kg)',
+                            hintStyle:
+                                TextStyle(fontSize: 22, color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(
+                    height: 40,
+                  ),
+                  //current height
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade600.withOpacity(0.6),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    width: 300,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                        child: TextField(
+                          style: TextStyle(
+                            fontSize: 22,
+                            color: Colors.white,
+                          ),
+                          controller: _currentHeight,
+                          decoration: InputDecoration(
+                            errorStyle: TextStyle(
+                                fontSize: 15, color: Colors.red[1000]),
+                            contentPadding: EdgeInsets.symmetric(vertical: 0),
+                            border: InputBorder.none,
+                            hintText: '   Current Height (Cm)',
+                            hintStyle:
+                                TextStyle(fontSize: 22, color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(
+                    height: 60,
+                  ),
+                  //calculate button
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 350,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                          child: GestureDetector(
+                            onTap: ()   {
+                              print('object');
+                              var bmiModel = BMIModel(
+                                  double.parse(_currentHeight.text),
+                                  double.parse(_currentWeight.text));
+
+
+                                widget.result =bmiModel.calculateBMI() as double;
+                                print('result in front : ' + '${widget.result}');
+                            },
+                            child: Container(
+                              padding: EdgeInsets.all(20),
+                              decoration: BoxDecoration(
+                                  color: Colors.teal.shade900,
+                                  borderRadius: BorderRadius.circular(16)),
+                              child: Center(
+                                child: Text(
+                                  'Calculate',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       ),
-    );
+    ]);
   }
 }
--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:my_diet_guide/screens/user_details_form.dart';
 import 'package:my_diet_guide/widgets/background_image.dart';
 import 'package:my_diet_guide/widgets/palatte.dart';
 
@@ -17,101 +18,69 @@ class _SignUpState extends State<SignUp> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
-  final _firstnameController = TextEditingController();
-  final _lastnameController = TextEditingController();
 
   bool error = false;
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    _emailController.dispose();
-    _passwordController.dispose();
-    _confirmPasswordController.dispose();
-    _firstnameController.dispose();
-    _lastnameController.dispose();
-    super.dispose();
-  }
+
 
   //function for sign up a user
-  Future signUpFunc() async{
-    if(passwordConfirmed()){
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-          email: _emailController.text.trim(),
-          password: _passwordController.text.trim());
-
-      //add user details
-      addUserDetails(
-          _firstnameController.text.trim(),
-          _lastnameController.text.trim(),
-          _emailController.text.trim());
-      showDialog(context: context, builder: (context){
-        return AlertDialog(
-            content: Text('User Added!'),
-            backgroundColor: Colors.green[200],
-            actions: <Widget>[
-              new MaterialButton(
-                color: Colors.green,
-                child: new Text("OK", style: TextStyle(color: Colors.white),),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ]
-        );
-      });
+  //function for sign up a user
+  Future signUpFunc() async {
+    if (passwordConfirmed()) {
+      String email = _emailController.text.trim();
+      String password = _passwordController.text.trim();
+      Navigator.push(context, MaterialPageRoute(builder: (context) =>
+          UserDetailsForm(email: email, password: password)));
     }
-
   }
-  Future addUserDetails(
-      String firstname, String lastname, String email
-      ) async {
-    await FirebaseFirestore.instance.collection('user').add(
-      {
-        'first name' : firstname,
-        'last name' : lastname,
-        'email' : email
+
+    bool passwordConfirmed() {
+      if (_passwordController.text.trim() ==
+          _confirmPasswordController.text.trim()) {
+        return true;
+      } else {
+        showDialog(context: context, builder: (context) {
+          return AlertDialog(
+              content: Text('Passwords doesn\'t match!'),
+              backgroundColor: Colors.red[200],
+              actions: <Widget>[
+                new MaterialButton(
+                  color: Colors.red,
+                  child: new Text("OK", style: TextStyle(color: Colors.white),),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ]
+          );
+        });
+        return false;
       }
-    );
-  }
-
-  bool passwordConfirmed(){
-    if(_passwordController.text.trim() == _confirmPasswordController.text.trim()){
-      return true;
-    }else{
-      showDialog(context: context, builder: (context){
-        return AlertDialog(
-            content: Text('Passwords doesn\'t match!'),
-            backgroundColor: Colors.red[200],
-            actions: <Widget>[
-              new MaterialButton(
-                color: Colors.red,
-                child: new Text("OK", style: TextStyle(color: Colors.white),),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ]
-        );
-      });
-      return false;
     }
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children:[
-        BackgroundImage(),
-        Scaffold(
-          backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          title: const Text('Sign Up',),
-        ),
+    @override
+    void dispose() {
+      // TODO: implement dispose
+      _emailController.dispose();
+      _passwordController.dispose();
+      _confirmPasswordController.dispose();
+      super.dispose();
+    }
 
-        body: SafeArea(
-            child: SingleChildScrollView(
-              child:
+    @override
+    Widget build(BuildContext context) {
+      return Stack(
+          children: [
+            BackgroundImage(),
+            Scaffold(
+              backgroundColor: Colors.transparent,
+              appBar: AppBar(
+                backgroundColor: Colors.transparent,
+                title: const Text('Sign Up',),
+              ),
+
+              body: SafeArea(
+                child: SingleChildScrollView(
+                  child:
                   Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
@@ -121,83 +90,13 @@ class _SignUpState extends State<SignUp> {
                       Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Text('Sign Up',
-                          style: HeadingTitle
-                        ),
-                      ),
-
-                      SizedBox(height: 50,),
-                      //First Name
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Colors.grey.shade600.withOpacity(0.6),
-                              borderRadius: BorderRadius.circular(16),
-                            ),
-                            width: 300,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Center(
-                                child: TextField(
-                                  style: TextStyle(
-                                    fontSize: 22,
-                                    color: Colors.white,
-                                  ),
-                                  controller: _firstnameController,
-                                  decoration: InputDecoration(
-                                      errorStyle: TextStyle(
-                                          fontSize: 15,
-                                          color: Colors.red[1000]
-                                      ),
-                                      contentPadding: EdgeInsets.symmetric(vertical: 0),
-                                      border: InputBorder.none,
-                                      hintText: '   First Name',
-                                      hintStyle: TextStyle(
-                                          fontSize: 22,
-                                          color: Colors.white
-                                      ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-
-                      SizedBox(height: 25,),
-                      //Last Name
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.grey.shade600.withOpacity(0.6),
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        width: 300,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Center(
-                            child: TextField(
-                              style: TextStyle(
-                                fontSize: 22,
-                                color: Colors.white,
-                              ),
-                              controller: _lastnameController,
-                              decoration: InputDecoration(
-                                errorStyle: TextStyle(
-                                    fontSize: 15,
-                                    color: Colors.red[1000]
-                                ),
-                                contentPadding: EdgeInsets.symmetric(vertical: 0),
-                                border: InputBorder.none,
-                                hintText: '   Last Name',
-                                hintStyle: TextStyle(
-                                    fontSize: 22,
-                                    color: Colors.white
-                                ),
-                              ),
-                            ),
-                          ),
+                            style: HeadingTitle
                         ),
                       ),
 
 
                       SizedBox(height: 25,),
-                          //input email
+                      //input email
                       Container(
                         decoration: BoxDecoration(
                           color: Colors.grey.shade600.withOpacity(0.6),
@@ -218,7 +117,8 @@ class _SignUpState extends State<SignUp> {
                                     fontSize: 15,
                                     color: Colors.red[1000]
                                 ),
-                                contentPadding: EdgeInsets.symmetric(vertical: 0),
+                                contentPadding: EdgeInsets.symmetric(
+                                    vertical: 0),
                                 border: InputBorder.none,
                                 hintText: '   Email',
                                 hintStyle: TextStyle(
@@ -254,7 +154,8 @@ class _SignUpState extends State<SignUp> {
                                     fontSize: 15,
                                     color: Colors.red[1000]
                                 ),
-                                contentPadding: EdgeInsets.symmetric(vertical: 0),
+                                contentPadding: EdgeInsets.symmetric(
+                                    vertical: 0),
                                 border: InputBorder.none,
                                 hintText: '   Password',
                                 hintStyle: TextStyle(
@@ -289,7 +190,8 @@ class _SignUpState extends State<SignUp> {
                                     fontSize: 15,
                                     color: Colors.red[1000]
                                 ),
-                                contentPadding: EdgeInsets.symmetric(vertical: 0),
+                                contentPadding: EdgeInsets.symmetric(
+                                    vertical: 0),
                                 border: InputBorder.none,
                                 hintText: '   Confirm Password',
                                 hintStyle: TextStyle(
@@ -308,34 +210,36 @@ class _SignUpState extends State<SignUp> {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           SizedBox(
-                            width : 350,
+                            width: 350,
                             child: Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                            child: GestureDetector(
-                              onTap: signUpFunc,
-                              child: Container(
-                                padding: EdgeInsets.all(20),
-                                decoration: BoxDecoration(color: Colors.teal.shade900,
-                                    borderRadius: BorderRadius.circular(16)),
-                                child: Center(
-                                  child: Text('Sign Up',
-                                    style: TextStyle(color: Colors.white,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 20),),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 25.0),
+                              child: GestureDetector(
+                                onTap: signUpFunc,
+                                child: Container(
+                                  padding: EdgeInsets.all(20),
+                                  decoration: BoxDecoration(
+                                      color: Colors.teal.shade900,
+                                      borderRadius: BorderRadius.circular(16)),
+                                  child: Center(
+                                    child: Text('Sign Up',
+                                      style: TextStyle(color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20),),
+                                  ),
                                 ),
                               ),
                             ),
-                    ),
                           ),
                         ],
                       ),
 
-                  ],
+                    ],
+                  ),
                 ),
-          ),
-      ),
-    ),
-  ]
-    );
+              ),
+            ),
+          ]
+      );
+    }
   }
-}
