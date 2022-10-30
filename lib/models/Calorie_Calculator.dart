41b68d7bcd80f54f72333dda84ffb34e39c53b6a
jananga99 <jananga1999@gmail.com>
2022-10-30 10:10:54 +0530

    Calorie burn functions

---
+++ lib/models/Calorie_Calculator.dart
@@ -0,0 +1,41 @@
+
+
+
+class Calorie_Calculator{
+
+  //https://www.healthline.com/health/fitness-exercise/how-many-calories-do-i-burn-a-day#calories-burned-by-activity
+  static Map<String, double>  activityLevelValues = {
+  'Not very active' :  1.2,
+  'Moderately active' : 1.55,
+  'Active' : 1.725
+  };
+
+  static double calculateRMR(gender, height, weight, age){
+    double val = 0;
+    if(gender == " Male"){
+      val = 9.99 * weight + 6.25 * height - 4.92 * age + 5;
+    }else if(gender == "Female"){
+      val = 9.99 * weight + 6.25 * height - 4.92 * age - 161;
+    }else{
+      throw Exception("Undefined gender at calculateBMR in CalorieCalculator");
+    }
+    return val;
+  }
+
+
+  static double calorieBurnPerDay(gender, height, weight, age, activeness){
+    if(!activityLevelValues.containsKey(activeness)){
+      throw Exception("Undefined activeness at calorieBurnPerDay in CalorieCalculator");
+    }
+    return calculateRMR(gender, height, weight, age) * activityLevelValues[activeness]!;
+  }
+
+
+  static double calorieBurn(gender, height, weight, age, activeness, numDays){
+    return numDays * calorieBurnPerDay(gender, height, weight, age, activeness);
+  }
+
+
+}
+
+
