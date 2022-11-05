6d3558071fee90ad2f24b701014cd4249605abfd
jananga99 <jananga1999@gmail.com>
2022-11-05 16:44:15 +0530

    erros in tests fixed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -13,7 +15,7 @@ import 'package:my_diet_guide/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp(firestore: FirebaseFirestore.instance, auth: FirebaseAuth.instance));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
