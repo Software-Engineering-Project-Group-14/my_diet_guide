7599dc9a3b1eebb5be826e9acfbc6c918e60dee9
Jalitha99 <jalithakalsara2@gmail.com>
2022-11-16 17:59:26 +0530

    stated web view diet

---
+++ lib/screens/web_screens/web_view_diet.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WebViewDietPlan extends StatefulWidget {
+  const WebViewDietPlan({Key? key}) : super(key: key);
+
+  @override
+  State<WebViewDietPlan> createState() => _WebViewDietPlanState();
+}
+
+class _WebViewDietPlanState extends State<WebViewDietPlan> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
